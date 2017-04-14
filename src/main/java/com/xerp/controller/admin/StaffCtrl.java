745fb172e4fca0803088f856154d83f6602a433b
shsun <shsun@shsuns-MacBook-Pro.local>
2017-04-14 17:28:00 +0800

    init

--- src/main/java/com/xerp/controller/admin/StaffCtrl.java
+++ src/main/java/com/xerp/controller/admin/StaffCtrl.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.xerp.dao.entity.StaffEty;
 import com.xerp.dao.mapper.base.StaffMapper;
+import com.xerp.pojo.PojoStaff;
+import com.xerp.pojo.PojoStaffExample;
+import com.xerp.pojo.PojoStaffMapper;
 
 import base.utils.json.IJsonResult;
 import base.utils.json.JsonResultFactory;
@@ -23,48 +26,74 @@ public class StaffCtrl {
 
 	@Autowired
 	private StaffMapper staffMapper;
-	
-	
+
+	@Autowired
+	private PojoStaffMapper pojoStaffMapper;
+
 	/**
 	 * 查询
 	 */
-	@RequestMapping(value="search")
+	/*
+	@RequestMapping(value = "search")
 	public @ResponseBody IJsonResult search(@RequestBody StaffEty staffEty) throws Exception {
-		int count = staffMapper.selectLimitCount(staffEty);
-		List<StaffEty> list = staffMapper.selectByLimit(staffEty);
+		// int count = staffMapper.selectLimitCount(staffEty);
+		// List<StaffEty> list = staffMapper.selectByLimit(staffEty);
+		// return JsonResultFactory.extgrid(list, count);
+
+		
+		PojoStaff ab;
+		
+		PojoStaffExample example = new PojoStaffExample();
+		int count = pojoStaffMapper.countByExample(example);
+		PojoStaffExample.Criteria criteria = example.createCriteria();
+		criteria.andNameEqualTo(staffEty.getName());
+		criteria.andNameIsNotNull();
+		List<PojoStaff> list = pojoStaffMapper.selectByExample(example);
 		return JsonResultFactory.extgrid(list, count);
 	}
+	*/
+	
+	@RequestMapping(value = "search")
+	public @ResponseBody IJsonResult search(@RequestBody PojoStaff staffEty) throws Exception {		
+		PojoStaffExample example = new PojoStaffExample();
+		int count = pojoStaffMapper.countByExample(example);
+		PojoStaffExample.Criteria criteria = example.createCriteria();
+		// criteria.andNameEqualTo(staffEty.getName());
+		// criteria.andNameIsNotNull();
+		List<PojoStaff> list = pojoStaffMapper.selectByExample(example);
+		return JsonResultFactory.extgrid(list, count);
+	}
+	
 	
 	/**
 	 * 保存
 	 */
-	@RequestMapping(value="save")
+	@RequestMapping(value = "save")
 	public @ResponseBody IJsonResult save(@RequestBody StaffEty staffEty) throws Exception {
-		if(staffEty.getId() == null) {
+		if (staffEty.getId() == null) {
 			staffMapper.insert(staffEty);
-		}
-		else {
+		} else {
 			staffMapper.updateById(staffEty);
 		}
 		return JsonResultFactory.success();
 	}
-	
+
 	/**
 	 * 删除
 	 */
-	@RequestMapping(value="delete")
+	@RequestMapping(value = "delete")
 	public @ResponseBody IJsonResult delete(@RequestParam("id") int id) {
 		staffMapper.deleteById(id);
 		return JsonResultFactory.success();
 	}
-	
+
 	/**
 	 * 得到详细信息
 	 */
-	@RequestMapping(value="getDetailInfo")
+	@RequestMapping(value = "getDetailInfo")
 	public @ResponseBody IJsonResult getDetailInfo(@RequestParam("id") int id) throws Exception {
 		StaffEty staffEty = staffMapper.selectById(id);
 		return JsonResultFactory.success(staffEty);
 	}
-	
+
 }
